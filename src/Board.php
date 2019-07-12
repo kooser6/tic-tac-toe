c486116d679c8f1a0d32b825e748c41841ec4ef2
omatamix <47761650+omatamix@users.noreply.github.com>
2019-07-12 17:31:17 -0500

    Add board handler.

---
+++ src/Board.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace Phoenix\Zero\TicTacToe;
+
+use function reset;
+use function array_push;
+
+/**
+ * @class Board.
+ */
+class Board
+{
+
+    private $board = [
+        '-', '-', '-',
+        '-', '-', '-',
+        '-', '-', '-',
+    ];
+
+    /**
+     * Reset the board.
+     *
+     * @return void Returns nothing.
+     */
+    public function reset(): void
+    {
+        $this->board = [
+            '-', '-', '-',
+            '-', '-', '-',
+            '-', '-', '-',
+        ];
+    }
+
+    /**
+     * Get the board.
+     *
+     * @return array The current tic tac toe board.
+     */
+    public function get(): array
+    {
+        return $this->board;
+    }
+
+    /**
+     * Set the board.
+     *
+     * @return void Returns nothing.
+     */
+    public function set(array $board): void
+    {
+        $this->board = $board;
+    }
+
+    public function convert(): array
+    {
+        $return = [];
+        reset($this->board);
+        foreach ($this->board as $square) {
+            if ($square == '-') {
+                array_push($this->board, 0);
+            } elseif ($square == 'x') {
+                array_push($this->board, 1);
+            } elseif ($square == 'o') {
+                array_push($this->board, 2);
+            } else {
+                throw new Exception\UnexpectedValueException('The board is corrupt.');
+            }
+        }
+        return $return;
+    }
+}
