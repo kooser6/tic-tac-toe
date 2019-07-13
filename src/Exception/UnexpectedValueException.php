1aafbdd6056bc6d54b6f9fa51683a052b3666ff6
omatamix <47761650+omatamix@users.noreply.github.com>
2019-07-13 03:57:31 -0500

    Add exception interface.

---
+++ src/Exception/UnexpectedValueException.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Phoenix\Zero\TicTacToe\Exception;
+
+use Throwable;
+
+/**
+ * @interface ExceptionInterface.
+ */
+interface ExceptionInterface extends Throwable
+{
+}
