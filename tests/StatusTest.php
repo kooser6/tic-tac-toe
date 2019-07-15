c57a9ef34a30696aebcfd28d4ef005708720a5ae
omatamix <47761650+omatamix@users.noreply.github.com>
2019-07-14 21:23:07 -0500

    Add more test assertions.

--- tests/StatusTest.php
+++ tests/StatusTest.php
@@ -1,1 +1,57 @@
+<?php declare(strict_types=1);
 
+namespace Phoenix\Zero\TicTacToe\Test;
+
+use PHPUnit\Framework\TestCase;
+
+use Phoenix\Zero\TicTacToe\Board;
+use Phoenix\Zero\NeuralNetwork\Classifier;
+
+use Phpml\Classification\MLPClassifier;
+
+/**
+ * @class StatusTest.
+ */
+class StatusTest extends TestCase
+{
+    /**
+     * @function testConstruction.
+     */
+    public function testConstruction()
+    {
+        $pre_classifier = new MLPClassifier(9, [1], ['x', 'o', '-']);
+        $classifier = new Classifier($pre_classifier);
+        $controller = new Game($classifier);
+        $status = $controller->getStatus();
+        $this->assertTrue(($status['status'] === 1));
+        $controller->moveAI();
+        $status = $controller->getStatus();
+        $this->assertTrue(($status['status'] === 1));
+        $controller->moveAI();
+        $status = $controller->getStatus();
+        $this->assertTrue(($status['status'] === 1));
+        $controller->moveAI();
+        $status = $controller->getStatus();
+        $this->assertTrue(($status['status'] === 1));
+        $controller->moveAI();
+        $status = $controller->getStatus();
+        $this->assertTrue(($status['status'] === 1));
+        $controller->moveAI();
+        $status = $controller->getStatus();
+        $this->assertTrue(($status['status'] === 1));
+        $controller->moveAI();
+        $status = $controller->getStatus();
+        $this->assertTrue(($status['status'] === 1));
+        $controller->moveAI();
+        $status = $controller->getStatus();
+        $this->assertTrue(($status['status'] === 1));
+        $controller->moveAI();
+        $status = $controller->getStatus();
+        $this->assertTrue(($status['status'] === 1));
+        $controller->moveAI();
+        $status = $controller->getStatus();
+        $this->assertTrue(($status['status'] === 0));
+        $controller->reset();
+        $this->assertTrue(($status['status'] === 1));
+    }
+}
